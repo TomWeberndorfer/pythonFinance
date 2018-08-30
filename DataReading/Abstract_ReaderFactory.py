c951294d39f3b5257718739bccb905ea3c8472b0
Tom <weberndorfer.thomas@gmail.com>
2018-08-30 22:18:11 +0200

    # implementation of fabric for data readers

--- DataReading/Abstract_ReaderFactory.py
+++ DataReading/Abstract_ReaderFactory.py
@@ -6,8 +6,6 @@ class Abstract_ReaderFactory:
                 reload_stockdata, parameter_dict):
         storage = self._create_data_reader(reader_to_create, stock_data_container_list,
                                            reload_stockdata, parameter_dict)
-        # TODO checken ob des e ned vl so ghert:
-        # result = storage.read_data()
         return storage
 
     @abstractmethod
